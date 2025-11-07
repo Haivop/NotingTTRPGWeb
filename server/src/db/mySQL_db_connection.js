7bf37574e258e3b55bdbbf192af608943c60f29b
Артем Свистун <ar.svistun@gmail.com>
2025-11-07 23:24:44 +0200

    security leak issue extermination

--- server/src/db/mySQL_db_connection.js
+++
@@ -1,6 +0,0 @@
-import { Database } from "./Database";
-
-const MySQLconfig = require("../../configs/mysql_db");
-const mySqlDb = new Database(MySQLconfig);
-
-module.exports = mySqlDb.getConnection();
